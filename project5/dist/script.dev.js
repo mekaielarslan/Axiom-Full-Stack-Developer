89176dc2d48dc15f9b4b6f3319b4d3ae8956d4b9
Mekaiel Arskan <mekaielarslan@gmail.com>
2022-05-29 23:26:35 +0500

    done

--- project5/dist/script.dev.js
+++ project5/dist/script.dev.js
@@ -1,7 +1,77 @@
 "use strict";
 
-var addUserBtn = document.getElementById();
-var doubleWealthBtn = document.getElementById();
-var filterWealthBtn = document.getElementById();
-var sortBtn = document.getElementById();
-var aggergateBtn = document.getElementById();
+// Get Dom
+var addUserBtn = document.getElementById('add-user');
+var doubleWealthBtn = document.getElementById('double-wealth');
+var filterWealthBtn = document.getElementById('filter-wealth');
+var sortBtn = document.getElementById('sort-wealth');
+var sumWealthBtn = document.getElementById('sum-wealth');
+var main = document.getElementById('main'); // Thios is the array to store user data to display in dom
+
+var userArray = []; // Function to fetch a random user and assign random wealth
+
+function generateRandomUser() {
+  var res, data, user, newUser;
+  return regeneratorRuntime.async(function generateRandomUser$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(fetch("https://randomuser.me/api"));
+
+        case 2:
+          res = _context.sent;
+          _context.next = 5;
+          return regeneratorRuntime.awrap(res.json());
+
+        case 5:
+          data = _context.sent;
+          // Save the user data 
+          user = data.results[0]; // Create new user  object with random user and wealth
+
+          newUser = {
+            user: "".concat(user.name.title, " ").concat(user.name.first, " ").concat(user.name.last),
+            wealth: Math.floor(Math.random() * 10000000)
+          }; //  Add newUser object to userArray
+
+          addUserData(newUser);
+
+        case 9:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
+
+; //Function to update the new user data from userArray
+
+function addUserData(user) {
+  //  Push user object to the userArray
+  userArray.push(user); // Update the DOM With new data in userArray
+
+  updateDOM();
+}
+
+; // Function to update the DOM with new user data from userArray
+
+function updateDOM() {
+  var userData = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : userArray;
+  // Wipe away content from main element
+  main.innerHTML = '<h2><strong>User</strong> Wealth</h2>'; // loop over  userData array and display user in the DOM
+
+  userData.forEach(function (user) {
+    // Create a new div element for the user
+    var divElement = document.createElement('div'); // Assign a class to the new div
+
+    divElement.classList.add('user');
+    divElement.innerHTML = "<strong>".concat(user.name, "</strong> \n       ").concat(formatNumberToDollar(user.wealth)); // Display the new divElement in the DOM
+
+    main.appendChild(divElement);
+  });
+}
+
+; // Generate some user on initial page load
+
+generateRandomUser(); // generateRandomUser();
+// generateRandomUser();
--- project5/script.js
+++ project5/script.js
@@ -1,5 +1,57 @@
-const addUserBtn = document.getElementById()
-const doubleWealthBtn  = document.getElementById()
-const filterWealthBtn = document.getElementById()
-const sortBtn = document.getElementById()
-const aggergateBtn = document.getElementById()
+// Get Dom
+const addUserBtn = document.getElementById('add-user');
+const doubleWealthBtn  = document.getElementById('double-wealth');
+const filterWealthBtn = document.getElementById('filter-wealth');
+const sortBtn = document.getElementById('sort-wealth');
+const sumWealthBtn = document.getElementById('sum-wealth');
+const main = document.getElementById('main');
+
+// Thios is the array to store user data to display in dom
+let userArray = [];
+
+// Function to fetch a random user and assign random wealth
+ async function generateRandomUser() {
+    // Use Fetch to get random user data from randomuser.me/api
+    const res = await fetch(`https://randomuser.me/api`); 
+    const data = await res.json();
+    // Save the user data 
+    const user = data.results[0];
+    // Create new user  object with random user and wealth
+     const newUser = {
+             user : `${user.name.title} ${user.name.first} ${user.name.last}`,
+             wealth : Math.floor( Math.random () * 10000000)
+     };
+    //  Add newUser object to userArray
+    addUserData(newUser);
+};
+
+ //Function to update the new user data from userArray
+ function addUserData (user) {
+   //  Push user object to the userArray
+   userArray.push (user);
+   // Update the DOM With new data in userArray
+     updateDOM();
+};
+
+// Function to update the DOM with new user data from userArray
+function updateDOM (userData = userArray){
+   // Wipe away content from main element
+   main.innerHTML = '<h2><strong>User</strong> Wealth</h2>'
+    // loop over  userData array and display user in the DOM
+    userData.forEach( user => {
+       // Create a new div element for the user
+       const divElement = document.createElement('div');
+       // Assign a class to the new div
+       divElement.classList.add('user');
+       divElement.innerHTML =  `<strong>${user.name}</strong> 
+       ${formatNumberToDollar(user.wealth)}`
+        // Display the new divElement in the DOM
+        main.appendChild(divElement);
+    })
+};
+
+// Generate some user on initial page load
+generateRandomUser();
+// generateRandomUser();
+// generateRandomUser();
+
